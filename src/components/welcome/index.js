b8a305de6c578b0a81f836c318afab8d36e6000e
Paul Hutchings <paul@xps>
2020-03-21 15:54:49 -0400

    Converted wlecome card to use built-in backdrop component

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -12,66 +12,78 @@ import {
   FormHelperText,
   Select,
   InputLabel,
-  MenuItem
+  MenuItem,
+  Backdrop
 } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-import '../app/App.css';
-import {
-  blueGrey
-} from "@material-ui/core/colors"
+import "../app/App.css";
+import { blueGrey } from "@material-ui/core/colors";
 
-const useStyles = makeStyles(theme => createStyles({
+const useStyles = makeStyles(theme =>
+  createStyles({
     formControl: {
-        margin: theme.spacing(1),
-        minWidth: 120,
-      },
-      selectEmpty: {
-        marginTop: theme.spacing(2),
-      }
-}));
+      margin: theme.spacing(1),
+      minWidth: 120
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2)
+    },
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: "#fff"
+    }
+  })
+);
 
 const Welcome = props => {
   const classes = useStyles();
 
-  const [school, setSchool] = React.useState('');
+  const [school, setSchool] = React.useState("");
+  const [open, setOpen] = React.useState(true);
 
-  const handleChange = event => {
+  const changeSchool = event => {
     setSchool(event.target.value);
+    closeBackdrop();
+  };
+
+  const closeBackdrop = () => {
+    setOpen(!open);
   };
 
   return (
-    <Grid className="canvas" container justify="center" alignItems="center">
-      <Grid item>
-      <Card>
-        <CardHeader title="Welcome to ScheduleMaker!" component="h1"/>
-        <CardContent>
-            <Typography align="center">
+    <Backdrop className={classes.backdrop} open={open}>
+      <Grid container justify="center" alignItems="center">
+        <Grid item>
+          <Card>
+            <CardHeader title="Welcome to ScheduleMaker!" component="h1" />
+            <CardContent>
+              <Typography align="center">
                 To begin, select your school.
-            </Typography>
-            
-        </CardContent>
-        <CardActions>
-        <Grid container justify="center">
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Grid container justify="center">
                 <FormControl className={classes.formControl}>
-                <InputLabel id="school-select-label">School</InputLabel>
-                <Select 
-                // native
-                value={school}
-                onChange={handleChange}
-                inputProps={{
-                    name: 'school',
-                    id :'school-select'
-                }} 
-                >
+                  <InputLabel id="school-select-label">School</InputLabel>
+                  <Select
+                    // native
+                    value={school}
+                    onChange={changeSchool}
+                    inputProps={{
+                      name: "school",
+                      id: "school-select"
+                    }}
+                  >
                     <MenuItem value=""></MenuItem>
-                  <MenuItem value={"temple"}>Temple University</MenuItem>
-                </Select>
-            </FormControl>
-            </Grid>
-        </CardActions>
-      </Card>
+                    <MenuItem value={"temple"}>Temple University</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </Backdrop>
   );
 };
 
