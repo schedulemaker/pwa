7fd8795a9a38443af81508c1650559a9c0e6bda4
Paul Hutchings <paul@xps>
2020-04-08 10:08:40 -0400

    Added blank calendar view

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { Grid } from "@material-ui/core";
 import Welcome from "../welcome";
 import Home from "../home";
+import Calendar from "../calendar";
 
 function App() {
   return (
     <Grid>
-      <Home></Home>
-      <Welcome></Welcome>
+      {/* <Home></Home>
+      <Welcome></Welcome> */}
+      <Calendar></Calendar>
     </Grid>
   );
 }
