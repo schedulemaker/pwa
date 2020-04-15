6d3e50eb08fe2c240b40fe7a6f08183cbd6d635b
Bradley Juma <bradleyjuma@temple.edu>
2020-04-15 00:32:21 -0400

    Auto complete search bar

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import "./App.css";
 import { Grid } from "@material-ui/core";
 import Welcome from "../welcome";
 import Home from "../home";
 import Calendar from "../calendar";
+import Labs from '../labs';
+
+ReactDOM.render(<Labs />, document.querySelector('#root'));
 
 function App() {
   return (
     <Grid>
       {/* <Home></Home>
       <Welcome></Welcome> */}
+      <Labs></Labs>
       <Calendar></Calendar>
     </Grid>
   );
