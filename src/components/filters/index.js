687b029108c016f042de56d4383490f715a161a5
Paul Hutchings <paul@xps>
2020-04-16 23:45:19 -0400

    Moved filter controls into seperate components

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,171 +1,26 @@
-import React from "react";
-import { 
-  Paper, 
-  Grid, 
-  Button,
-  ButtonGroup,
-  FormControl,
-  FormGroup,
-  FormControlLabel,
-  FormLabel,
-  Checkbox,
-  Radio,
-  Switch,
-  Slider,
-  Typography,
-  RadioGroup,
-} from "@material-ui/core";
-import {
-  withStyles,
-  ThemeProvider,
-  makeStyles,
-  useTheme,
-} from "@material-ui/core/styles";
-import MomentUtils from '@date-io/moment';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-} from '@material-ui/pickers';
-import {
-  getHoursMinutes,
-} from '../utils';
-import TransferList from '../transfer-list';
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
 
-const days = [
-    'Sun',
-    'Mon',
-    'Tues',
-    'Wed',
-    'Thurs',
-    'Fri',
-    'Sat',
-    'Sun'
-];
-
-
+import InstructorSearch from "../instructor-search";
+import TimeFilters from "../times";
+import Rankings from "../rankings";
+import DayFilters from "../days";
 
 export default function Filters(props) {
-  const [startDate, setStartDate] = React.useState(
-    new Date().setHours(...getHoursMinutes(props.minTime))
-  );
-  const [endDate, setEndDate] = React.useState(
-    new Date().setHours(...getHoursMinutes(props.maxTime))
-  );
-  const [density, setDensity] = React.useState("compact");
-  const [distance, setDistance] = React.useState("shortest");
-  const [checked, setChecked] = React.useState(false);
-
-  const handleCheckbox = (event) => {
-    setChecked(event.target.checked);
-  };
-
-  const handleDensity = (event) => {
-    setDensity(event.target.value);
-  };
-
-  const handleDistance = (event) => {
-    setDistance(event.target.value);
-  };
-
   return (
-    <Grid container justify="center" alignItems='center' spacing={3}>
-      <Grid container item justify="space-around">
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <KeyboardTimePicker
-            margin="normal"
-            id="time-picker"
-            label="Set desired start time"
-            value={startDate}
-            onChange={setStartDate}
-            KeyboardButtonProps={{
-              "aria-label": "change time",
-            }}
-          />
-          <KeyboardTimePicker
-            margin="normal"
-            id="time-picker"
-            label="Set desired end time"
-            value={endDate}
-            onChange={setEndDate}
-            KeyboardButtonProps={{
-              "aria-label": "change time",
-            }}
-          />
-        </MuiPickersUtilsProvider>
+    <Grid container justify="center" alignItems="center" spacing={3}>
+      <Grid container item justify="space-evenly">
+        <TimeFilters minTime={props.minTime} maxTime={props.maxTime} />
       </Grid>
-      <Grid container item justify='center' alignItems='center'>
-        <FormControl>
-          <FormLabel>Select desired days</FormLabel>
-          <FormGroup row>
-            {days.map((day) => (
-              <FormControlLabel
-                value={day}
-                control={
-                  <Checkbox
-                    checked={checked}
-                    onChange={handleCheckbox}
-                    name={day}
-                  />
-                }
-                label={day}
-                labelPlacement="top"
-              />
-            ))}
-          </FormGroup>
-        </FormControl>
+      <Grid container item justify="center" alignItems="center">
+        <DayFilters />
       </Grid>
-        <Grid container item justify='space-around'>
-        <Grid item>
-        <FormControl component="fieldset">
-          <FormLabel component="legend">Select schedule density</FormLabel>
-          <RadioGroup
-            aria-label="density"
-            name="density"
-            value={density}
-            onChange={handleDensity}
-          >
-            <FormControlLabel
-              value="compact"
-              control={<Radio />}
-              label="Compact"
-            />
-            <FormControlLabel
-              value="spread"
-              control={<Radio />}
-              label="Spread out"
-            />
-          </RadioGroup>
-        </FormControl>
+      <Grid container item justify="space-evenly">
+        <Rankings />
       </Grid>
-      <Grid item>
-      <FormControl component="fieldset">
-          <FormLabel component="legend">Select distance ranking</FormLabel>
-          <RadioGroup
-            aria-label="distance"
-            name="distance"
-            value={distance}
-            onChange={handleDistance}
-          >
-            <FormControlLabel
-              value="shortest"
-              control={<Radio />}
-              label="Shortest"
-            />
-            <FormControlLabel
-              value="longest"
-              control={<Radio />}
-              label="Longest"
-            />
-          </RadioGroup>
-        </FormControl>
+      <Grid container item justify="center">
+        <InstructorSearch school={props.school} term={props.term} />
       </Grid>
-      <Grid container item justify='center'>
-        <FormControl>
-          <FormLabel>Select preferred professors</FormLabel>
-          <TransferList school={props.school} term={props.term} />
-        </FormControl>
-      </Grid>
-        </Grid>
     </Grid>
   );
 }
