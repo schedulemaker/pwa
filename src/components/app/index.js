f9f7ffcdb37d7d258c5cab70abe487d5f9b73a0a
Paul Hutchings <paul@xps>
2020-04-19 12:18:06 -0400

    Simplified imports

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,21 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  Container
+  AppBar,
+  Typography,
+  Button,
+  Grid,
+  CssBaseline,
+  Toolbar,
+  IconButton
 } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import {
+  Menu as MenuIcon
+} from '@material-ui/icons';
 import Routes from "../../Routes";
 import { AppContext } from "../../libs/contextLib";
 import { Auth } from "aws-amplify";
 import {useHistory} from "react-router-dom";
 import { Link as RouterLink } from 'react-router-dom';
 import { onError } from "../../libs/errorLib";
-import { Grid, CssBaseline } from '@material-ui/core';
 import BotNav from '../bottom-nav';
 import ScheduleView from '../schedule-view';
 import Filters from '../filters';
