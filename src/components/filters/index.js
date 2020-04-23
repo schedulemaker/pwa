c575f68e68737dcaec07179894c9394874a05a3e
Paul Hutchings <paul@xps>
2020-04-23 11:27:18 -0400

    filter layout attempts

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -8,38 +8,41 @@ import DayFilters from "../days";
 
 export default function Filters(props) {
   return (
-    <Grid container justify="center">
-      {/* {props.count > 0 ? ( */}
-        <Grid container direction='column' justify="space-between" alignItems="center">
+    <Grid container direction="column" justify="space-evenly">
+        <Grid container item justify="center">
           <Alert severity="success">
-            <AlertTitle>{props.count} schedule{props.count !== 1 && ('s')} found</AlertTitle>
+            <AlertTitle>
+              {props.count} schedule{props.count !== 1 && "s"} found
+            </AlertTitle>
             Use the options below to customize your schedule
           </Alert>
-          <Grid container item justify="space-evenly">
-            <TimeFilters
-              times={props.times}
-              changeFilters={props.changeFilters}
-            />
-          </Grid>
-          <Grid container item justify="center" alignItems="center">
-            <DayFilters changeFilters={props.changeFilters} days={props.days} />
-          </Grid>
-          <Grid container item justify="space-evenly">
-            <Rankings
-              changeFilters={props.changeFilters}
-              rankings={props.rankings}
-            />
-          </Grid>
-          <Grid container item justify="center">
-            <InstructorSearch
-              school={props.school}
-              term={props.term}
-              instructors={props.instructors}
-              changeFilters={props.changeFilters}
-              instructorFilter={props.instructorFilter}
-            />
-          </Grid>
         </Grid>
+        <Grid container item justify="space-evenly">
+          <TimeFilters
+            times={props.times}
+            changeFilters={props.changeFilters}
+          />
+        </Grid>
+        <Grid container item justify="center" alignItems="center">
+          <DayFilters changeFilters={props.changeFilters} days={props.days} />
+        </Grid>
+        <Grid container item justify="space-evenly">
+          <Rankings
+            changeFilters={props.changeFilters}
+            rankings={props.rankings}
+          />
+        </Grid>
+        <Grid container item justify="center">
+          <InstructorSearch
+            school={props.school}
+            term={props.term}
+            instructors={props.instructors}
+            changeFilters={props.changeFilters}
+            instructorFilter={props.instructorFilter}
+          />
+        </Grid>
+        {/* {props.count > 0 ? (
+    
       {/* ) : (
         
       )} */}
@@ -47,6 +50,7 @@ export default function Filters(props) {
           <AlertTitle>No schedules found</AlertTitle>
           Try changing some of the options below
         </Alert> */}
-    </Grid>
+      </Grid>
+      
   );
 }
